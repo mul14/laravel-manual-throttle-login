3804ea9d5a219b6759f2bb4658a7bc7691804423
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-01-14 16:46:36 +0700

    Add throttle login implementation

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,36 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Cache\RateLimiter;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+// Inject RateLimiter
+Route::post('login', function (RateLimiter $limiter)
+{
+    $ip = request()->ip();
+    $maxLogin = 5;
+    $decay = 0.1; // In minutes
+
+    if ($limiter->tooManyAttempts($ip, $maxLogin, $decay))
+    {
+        return back()->withMessage(
+            "Too many login. Please wait in {$limiter->availableIn($ip)} second(s)."
+        );
+    }
+
+    $limiter->hit($ip, $decay);
+
+    $credentials = request()->only('email', 'password');
+
+    if (!auth()->attempt($credentials)) {
+        return back()->withMessage(
+            "Invalid credentials. Retries left: " . $limiter->retriesLeft($ip, $maxLogin)
+        );
+    }
+
+    $limiter->clear($ip);
+
+    return 'Congratulations!';
 });
